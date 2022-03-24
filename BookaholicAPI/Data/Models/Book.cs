e5d98e81129d776e529148df6d4cd7d2f8f2c17b
SimonaMDimitrova <simona.m.dimitrova@gmail.com>
2022-03-24 15:20:31 +0200

    App initialization

---
+++ BookaholicAPI/Data/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookaholicAPI.Data.Models
+{
+    public class Book
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public byte Rating { get; set; }
+
+        public string? Description { get; set; }
+
+        public short Pages { get; set; }
+
+        public Language Language { get; set; }
+
+        public string? LanguageId { get; set; }
+
+        public ICollection<Genre>? Genres { get; set; }
+
+        public Author Author { get; set; }
+        
+        public string? AuthorId { get; set; }
+
+        public ICollection<Comment>? Comments { get; set; }
+    }
+}
---
+++ BookaholicAPI/Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookaholicAPI.Data.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public Country Country { get; set; }
+
+        public string? CountryId { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public ICollection<IdentityUser>? Users { get; set; }
+    }
+}
